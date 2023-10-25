736e9a892c9fd06c3c8460b6346dfa15d6bcc137
jooooonj <lee22424205@naver.com>
2023-10-25 23:59:26 +0900

    프로젝트 세팅

---
+++ src/main/java/com/project/guakamole/GuakamoleApplication.java
@@ -0,0 +1,13 @@
+package com.project.guakamole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuakamoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuakamoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/guakamole/GuakamoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.guakamole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuakamoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
