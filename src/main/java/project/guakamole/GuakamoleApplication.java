5bb6d9b930bce876fb540f0248526d887c67103a
jooooonj <lee22424205@naver.com>
2023-10-27 22:13:42 +0900

    feat : 엔티티 맵핑

--- src/main/java/project/guakamole/GuakamoleApplication.java
+++ src/main/java/project/guakamole/GuakamoleApplication.java
@@ -9,5 +9,4 @@ public class GuakamoleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GuakamoleApplication.class, args);
 	}
-
 }
--- src/main/java/project/guakamole/domain/violation/entity/Violation.java
+++ src/main/java/project/guakamole/domain/violation/entity/Violation.java
@@ -1,4 +1,36 @@
 package project.guakamole.domain.violation.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.guakamole.domain.copyright.entity.Copyright;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
 public class Violation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "copyright_id")
+    private Copyright copyright;
+
+    @Column(name = "violator_name", nullable = false)
+    private String violatorName;
+
+    @Column(name = "violate_date", nullable = false)
+    private LocalDateTime violateDate;
+
+    @Column(name = "violate_react_level", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ViolateReactLevel reactLevel;
 }
