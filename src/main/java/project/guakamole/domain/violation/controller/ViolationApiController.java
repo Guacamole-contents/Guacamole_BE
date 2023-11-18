b154725877530ab59aa834dcd4118af4b13b42cd
jooooonj <lee22424205@naver.com>
2023-11-19 00:19:13 +0900

    feat: 침헤 데이터 상세 api 작성

--- src/main/java/project/guakamole/domain/violation/controller/ViolationApiController.java
+++ src/main/java/project/guakamole/domain/violation/controller/ViolationApiController.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.guakamole.domain.violation.dto.request.CreateViolationRequest;
+import project.guakamole.domain.violation.dto.request.UpdateReactLevelRequest;
+import project.guakamole.domain.violation.dto.response.DetailViolationResponse;
 import project.guakamole.domain.violation.dto.response.FindViolationResponse;
 import project.guakamole.domain.violation.service.ViolationService;
 
@@ -27,9 +29,31 @@ public class ViolationApiController {
     }
 
     @GetMapping("/violations")
-    public ResponseEntity<List<FindViolationResponse>> findViolations(@RequestParam(name = "page", defaultValue = "0") Integer page){
-        Pageable pageable = PageRequest.of(page, 10);
+    public ResponseEntity<List<FindViolationResponse>> findViolations(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "size", defaultValue = "6") Integer size)
+    {
+        Pageable pageable = PageRequest.of(page, size);
 
         return ResponseEntity.ok(violationService.findViolations(pageable));
     }
+
+    @GetMapping("/violation/{violationId}")
+    public ResponseEntity<DetailViolationResponse> findViolation(
+            @PathVariable("violationId") Long violationId
+    )
+    {
+        return ResponseEntity.ok(violationService.findViolation(violationId));
+    }
+
+    @PutMapping("/violation/{violationId}")
+    public ResponseEntity<Void> updateReactLevel(
+            @PathVariable("violationId") Long violationId,
+            @RequestBody UpdateReactLevelRequest request)
+    {
+        violationService.updateReactLevel(request, violationId);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
