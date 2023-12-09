8a33c72c3db4c06ef67116a1a4c366fcf0f2fe2f
jooooonj <lee22424205@naver.com>
2023-12-09 15:13:52 +0900

    refactor: 신청자 인증 이미지 엔티티 추가 및 승인 시 신청자 정보 전달하기 위한 Dto 작성

---
+++ src/main/java/project/guakamole/domain/applicant/dto/ApplicantInfoDto.java
@@ -0,0 +1,12 @@
+package project.guakamole.domain.applicant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApplicantInfoDto {
+    private String creatorName;
+    private String email;
+    private String chanelLink;
+}
