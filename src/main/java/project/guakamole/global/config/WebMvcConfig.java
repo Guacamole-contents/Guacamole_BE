6cf39ef4419cdd267009a85597155c33cb109799
jooooonj <lee22424205@naver.com>
2024-01-05 00:37:34 +0900

    fix: security cors 이슈

---
+++ src/main/java/project/guakamole/global/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+//package project.guakamole.global.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                // 모든 경로에 대해
+//                registry.addMapping("/**")
+//                        .exposedHeaders("*")
+//                        .allowedOriginPatterns("*")
+//                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true)
+//                        .maxAge(3600L);
+//            }
+//        };
+//    }
+//}
